ab45491bc63997685b5d8a183b625a05432e9a57
MucaSan <villar.mucasan@gmail.com>
2025-05-11 23:50:11 -0300

    Adicionando implementação base dos endpoints de usuário

---
+++ backend/app/model/usuario.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.sql import func
+from config.database import Base
+
+class Usuario(Base):
+    __tablename__ = "usuario"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(255), nullable=False)
+    email = Column(String(64), nullable=False, unique=True)
+    senha = Column(String(64), nullable=False)  
+    data_nascimento = Column(Date, nullable=False)
