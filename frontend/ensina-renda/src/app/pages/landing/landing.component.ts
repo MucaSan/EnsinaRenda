68f340d60719eacc42984727147d2800861674ac
Kaua <kaua.ferreiracavalcante@gmail.com>
2025-05-23 09:08:15 -0300

    Add footer, fix layout issue in FAQ, add animation on hover buttons

--- frontend/ensina-renda/src/app/pages/landing/landing.component.ts
+++ frontend/ensina-renda/src/app/pages/landing/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../shared/footer/footer.component';
 import { FaqComponent } from './components/faq/faq.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [FaqComponent],
+  imports: [FaqComponent, FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
