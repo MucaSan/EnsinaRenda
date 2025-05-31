a2d5c5f3873b799f503491e97d13f2a25017b309
MucaSan <villar.mucasan@gmail.com>
2025-05-31 19:24:00 -0300

    [Infra] Adicionando um reverse proxy conversão de requisições HTTP para gRPC e implementação de uma docs para a API.

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,28 +1,79 @@
 package main
 
 import (
+	"context"
 	pb "ensina-renda/adapter/grpc/pb"
 	"ensina-renda/adapter/grpc/service"
 	"ensina-renda/adapter/grpc/service/container"
 	"ensina-renda/config/interceptor"
 	controller "ensina-renda/controller/usuario"
 	"ensina-renda/repository"
+	"flag"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"sync"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 /*
-	Esse é o arquivo principal do projeto, responsável por iniciar o servidor gRPC e registrar os interceptors.
+	Esse é o arquivo principal do projeto, responsável por iniciar paralelamente em uma única função o proxy reverso HTTP e o servidor gRPC.
 */
 
 func main() {
-	// Faz com que o processo comece a ouvir através do protocolo TCP-IP na porta 50051.
-	lis, err := net.Listen("tcp", ":50051")
+	// Inicia um canal de erros com tamanho 2
+	canalErro := make(chan error, 2)
+
+	// Inicialização da variável de grupo de espera
+	var wg sync.WaitGroup
+	// Adiciona duas go rotinas para o processo de paralelismo
+	wg.Add(2)
+
+	// Roda o servidor gRPC em paralelo
+	go func() {
+		defer wg.Done()
+
+		log.Println("Subindo servidor gRPC em localhost:9090")
+		err := subirServidorGRPC()
+		if err != nil {
+			canalErro <- err
+		}
+	}()
+
+	// Roda o proxy reverso HTTP em paralelo
+	go func() {
+		defer wg.Done()
+
+		log.Println("Subindo proxy reverso em localhost:8081")
+		err := subirProxyReversoHTTP()
+		if err != nil {
+			canalErro <- err
+		}
+	}()
+
+	// Caso ocorra um erro, só entrará nos logs de parada caso os dois falhem.
+	wg.Wait()
+
+	err := <-canalErro
 	if err != nil {
-		fmt.Printf("\nCouldn't create listener for localhost port :50051. \nError found: %s", err.Error())
+		log.Printf("Erro encontrado: %v \n", err.Error())
+		return
+	}
+
+	log.Printf("Os servidores tiveram uma conclusão inesperada.")
+
+}
+
+// Sobe o servidor gRPC para processar as requisições do frontend e interagir diretamente com o banco de dados.
+func subirServidorGRPC() error {
+	// Faz com que o processo comece a ouvir através do protocolo TCP-IP na porta 9090.
+	lis, err := net.Listen("tcp", ":9090")
+	if err != nil {
+		fmt.Printf("\nNao foi possivel criar um listener para o localhost na :9090. \nErro encontrado: %s", err.Error())
 	}
 
 	// Inicializa os repositórios para serem utilizados pelos controllers.
@@ -32,15 +83,34 @@ func main() {
 	container := container.NewEnsinaRendaContainer(controller.NewUsuarioController(usuarioRepository))
 
 	// Registra, respectivamente, o servidor gRPC (com os interceptors nele) e o serviço com a API gRPC.
-	grpcServer := grpc.NewServer(
+	servidorGrpc := grpc.NewServer(
 		grpc.UnaryInterceptor(interceptor.DatabaseUnaryInterceptor),
 	)
 	ensinaRendaService := service.NewEnsinaRendaService(container)
-	pb.RegisterEnsinaRendaServiceServer(grpcServer, ensinaRendaService)
+	pb.RegisterEnsinaRendaServiceServer(servidorGrpc, ensinaRendaService)
 
-	//Disponibiliza o servidor para ser consumido.
-	log.Println("Server started on port 50051.")
-	if err := grpcServer.Serve(lis); err != nil {
-		fmt.Printf("\nCouldn't serve with provided listener.\nError found: %s", err.Error())
+	// Inicializa o servidor gRPC
+	return servidorGrpc.Serve(lis)
+}
+
+// Sobe o proxy reverso, que serve como uma abstração para a camada do servidor gRPC, basicamente, transformando requisições HTTP para gRPC.
+func subirProxyReversoHTTP() error {
+	// Porta de entrada para o endpoint de registro do proxy reverso HTTP.
+	endpointservidorGrpc := flag.String("endpoint-servidor-grpc", "localhost:9090", "endpoint de entrada para o servidor gRPC")
+
+	// Cria o canal de comunicação do contexto, que interpreta a requisição do frontend e embeda isso na variável a fim de manuntenção das ações necessárias para o backend.
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	//Registra o servidor MUX, com a comunicação TSL/SSL inativada.
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := pb.RegisterEnsinaRendaServiceHandlerFromEndpoint(ctx, mux, *endpointservidorGrpc, opts)
+	if err != nil {
+		return err
 	}
+
+	// Inicializa o servidor do proxy reverso com a configuração do MUX na porta 8081 (evitar subir o frontend e o backend na mesma porta)
+	return http.ListenAndServe(":8081", mux)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -3,8 +3,11 @@ module ensina-renda
 go 1.23.4
 
 require (
-	google.golang.org/grpc v1.69.2
-	google.golang.org/protobuf v1.36.1
+	github.com/google/uuid v1.6.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3
+	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb
+	google.golang.org/grpc v1.72.2
+	google.golang.org/protobuf v1.36.5
 	gorm.io/gorm v1.25.10
 )
 
@@ -12,7 +15,6 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
@@ -26,9 +28,9 @@ require (
 require (
 	github.com/go-playground/validator/v10 v10.26.0
 	github.com/jackc/pgx/v5 v5.7.5 // indirect
-	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
 	gorm.io/driver/postgres v1.5.11
 )
